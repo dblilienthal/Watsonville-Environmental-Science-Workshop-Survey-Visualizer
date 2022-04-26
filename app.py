33e2da0b935ce114bd2bcc6594baf3c493a13450
Derek Lilienthal <dlilienth@gmail.com>
2022-04-25 22:51:05 -0700

    Adding more

--- app.py
+++ app.py
@@ -1,7 +1,3 @@
-from enum import Enum
-from io import BytesIO, StringIO
-from typing import Union
-
 import pandas as pd
 import streamlit as st
 
@@ -17,30 +13,44 @@ img {
  
 class FileUpload(object):
  
-    def __init__(self):
-        self.fileTypes = ["csv", "png", "jpg"]
- 
     def run(self):
         """
         Upload File on Streamlit Code
         :return:
         """
-        st.info(__doc__)
         st.markdown(STYLE, unsafe_allow_html=True)
-        file = st.file_uploader("Upload file", type=self.fileTypes)
+        file = st.file_uploader("Upload file", type=['csv'])
         show_file = st.empty()
         if not file:
-            show_file.info("Please upload a file of type: " + ", ".join(["csv", "png", "jpg"]))
-            return
-        content = file.getvalue()
-        # if isinstance(file, BytesIO):
-        #     show_file.image(file)
-        # else:
-        data = pd.read_csv(file)
-        st.dataframe(data.head(10))
-        file.close()
- 
+            show_file.info("Please upload a file of type: csv")
+            return pd.DataFrame() # Returns a blank dataframe to avoid error. Might want to fix later 
+        else:
+            data = pd.read_csv(file)
+            file.close()
+
+            return data
+
+
  
 if __name__ ==  "__main__":
+
+    st.header("Watsonville Environmental Workshop Monthly Report Visualizer")
+
     helper = FileUpload()
-    helper.run()
+    data = helper.run()
+    # If the data has been populated
+    if data.shape != (0,0):
+        st.dataframe(data.head(10))
+
+        ### Sidebar code
+        st.sidebar.header('Features to graph')
+        column = st.sidebar.selectbox("Target Column", data.columns.tolist())
+        timeseries_column = st.sidebar.selectbox("Time Column", data.columns.tolist())
+        print("Selected columns:", column)
+        freq = st.sidebar.selectbox("Frequency", ['Week', 'Month', 'Year', 'All'])
+        print("Selected Frequency:", freq)
+
+
+
+
+
