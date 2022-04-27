b3aee4fa8f5042effa6cc081e213a742860e680d
Derek Lilienthal <dlilienth@gmail.com>
2022-04-26 22:22:21 -0700

    Adding new stuff

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+from re import S
 import pandas as pd
 import streamlit as st
+import matplotlib.pyplot as plt
 
  
 STYLE = """
@@ -47,11 +49,44 @@ if __name__ ==  "__main__":
 
         ### Sidebar code
         st.sidebar.header('Features to graph')
-        column = st.sidebar.selectbox("Target Column", data.columns.tolist())
-        timeseries_column = st.sidebar.selectbox("Time Column", data.columns.tolist())
-        print("Selected columns:", column)
-        freq = st.sidebar.selectbox("Frequency", ['Week', 'Month', 'Year', 'All'])
-        print("Selected Frequency:", freq)
+        st.sidebar.subheader('Timestamp column')
+        timeseries_column = st.sidebar.selectbox("Time Column (The column that has the time the survey was taken)", data.columns.tolist())
+        st.sidebar.subheader('Target Column')
+        column = st.sidebar.selectbox("The column you want to graph", data.columns.tolist())
+        st.sidebar.subheader('Frequency')
+        freq = st.sidebar.radio("Frequency", ['Week', 'Month', 'Year', 'All'])
+        #st.sidebar.subheader(f'Number of {freq}\'s')
+        num_freq = ''
+        top_num_freq = data.shape[1]
+        if freq != 'All':
+            num_freq = st.sidebar.slider(f'Number of {freq}\'s', 1, 12)
+
+            top_num_freq = st.sidebar.slider(f'Top number of results', 1, 30)
+
+
+        # Click for button
+        clicked = st.sidebar.button('Make Graph')
+        if clicked:
+            print("Selected columns:", column)
+            print('Time column:', timeseries_column)
+            print("Selected Frequency:", freq)
+            print("Number of freqs:",num_freq)
+
+            # Convert the timecolumn to datetime
+            data[timeseries_column] = pd.to_datetime(data[timeseries_column])
+            data.set_index(timeseries_column)
+
+            # Getting the data
+            val_count_df = data[column].value_counts()[:top_num_freq]  # Geting value counts by selected column
+            val_count_df.sort_values(ascending=True, inplace=True)
+            st.dataframe(val_count_df)
+
+            fig, ax = plt.subplots()
+            ax.barh(val_count_df.index, val_count_df.values)
+
+            st.pyplot(fig)
+
+
 
 
 
