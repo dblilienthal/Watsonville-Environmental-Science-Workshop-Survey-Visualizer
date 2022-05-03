704ed13e25154503a8b6b1b04452f08cbd993971
Derek Lilienthal <dlilienth@gmail.com>
2022-05-03 11:22:44 -0700

    Final push

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-from re import S
-from numpy import size
 import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
@@ -42,51 +40,53 @@ if __name__ ==  "__main__":
     # Title
     st.header("Watsonville Environmental Workshop Survey Visualizer")
 
-    helper = FileUpload()
+    helper = FileUpload() # Loading in the CSV file
     data = helper.run()
     # If the data has been populated
     if data.shape != (0,0):
 
         ### Snapshot of the dataset
-        st.caption('Snapshot of uploaded dataset')
-        st.dataframe(data.head(10))
+        st.subheader('Snapshot of uploaded dataset')
+        st.caption('Last 10 results')
+        st.dataframe(data.tail(10)) # Shows last 10 results
 
         ### Sidebar code
         st.sidebar.header('Features to graph')
         st.sidebar.subheader('Timestamp column')
-        timeseries_column = st.sidebar.selectbox("Time Column (The column that has the time the survey was taken)", data.columns.tolist())
+        timeseries_column = st.sidebar.selectbox("The column that has the time the survey was taken", data.columns.tolist()) # Shows all the columns in the dataset
         st.sidebar.subheader('Target Column')
-        column = st.sidebar.selectbox("The column you want to graph", data.columns.tolist())
-        
-        st.sidebar.subheader('Date Input')
-        start_date = st.sidebar.date_input('Start date')
-        end_date = st.sidebar.date_input('End date')
+        column = st.sidebar.selectbox("The column you want to graph", data.columns.tolist()) # Shows all the columns in the dataset
+        st.sidebar.subheader('Date Range')
+        start_date = st.sidebar.date_input('Start date') # Start date
+        end_date = st.sidebar.date_input('End date') # End Date
 
         # Check to make sure the dates are okay
         if (start_date > end_date):
             st.sidebar.header('Please fix the dates. Start date is greater than end date.')
 
-        print(start_date)
-
+        # Showing the top n results (5 is default)
         st.sidebar.subheader('Number of results')
-        top_num_freq = st.sidebar.slider(f'Top number of results', 1, 30)
+        top_num_freq = st.sidebar.slider(f'Top number of results', 1, 30, 5)
 
+        # Graphing options
         st.sidebar.subheader('Graphing Options')
-        width = st.sidebar.slider("plot width", 1, 25, 5)
-        height = st.sidebar.slider("plot height", 1, 25, 5)
-        title_font_size = st.sidebar.slider("Title Size", 1, 50, 10)
-        axis_label_size = st.sidebar.slider("X/Y Label Size", 1, 30, 10)
+        width = st.sidebar.slider("plot width", 1, 25, 5) # Width
+        height = st.sidebar.slider("plot height", 1, 25, 5)  # Height
+        title_font_size = st.sidebar.slider("Title Size", 1, 50, 10) # Font size
+        axis_label_size = st.sidebar.slider("X/Y Label Size", 1, 30, 10) # Axis label size
         
         # Click for button 
         clicked = st.sidebar.button('Make Graph')
-        if clicked:
+        ### If the button has been clicked
+        if clicked: 
             print("Selected columns:", column)
             print('Time column:', timeseries_column)
 
             data[timeseries_column] = pd.to_datetime(data[timeseries_column]) # Convert the timecolumn to datetime
             data = data[(data[timeseries_column] >= str(start_date)) & (data[timeseries_column] <= str(end_date))] # Filter by dates
 
-            # Getting the data
+            # Putting the data into a dictionary by times it appears.
+            # This also accounts for values seperated by commas
             result_dict = {}
             for row in data[column].astype(str):
                 try:
@@ -96,24 +96,25 @@ if __name__ ==  "__main__":
                 except:
                     pass
                 
-            result_dict = dict(sorted(result_dict.items(), key=lambda item: item[1], reverse=False))
-            print(result_dict)
-            val_count_df = pd.DataFrame.from_dict(result_dict, orient='index')[:top_num_freq]
-            # val_count_df = data[column].astype(str).value_counts()[:top_num_freq]  # Geting value counts by selected column
-            # val_count_df.sort_values(ascending=True, inplace=True)
-            st.dataframe(val_count_df)
+            result_dict = dict(sorted(result_dict.items(), key=lambda item: item[1], reverse=False)) # Reversing the dictionary to start with higher counts first
+            val_count_df = pd.DataFrame.from_dict(result_dict, orient='index')[:top_num_freq] # Getting the top N results
+            y_values = val_count_df.index.tolist() # Converting the y-values to list 
+            x_values = val_count_df.values.flatten() # Converting the x-values to list
+            print(result_dict, y_values, x_values, sep='\n')
 
-            y_values = val_count_df.index.tolist()
-            x_values = val_count_df.values.flatten()
+            ### Graph code
+            st.subheader('Graph')
+            fig, ax = plt.subplots(figsize=(width, height)) # Creating the subplot
+            ax.barh(y_values, x_values) # Adding the values to the plot
+            ax.set_title(f'{column}', size=title_font_size) # Creating the title
+            ax.set_xticks(range(max(x_values)+1)) # Setting the x-labels
+            ax.set_xticklabels(range(max(x_values)+1), size=axis_label_size) # Setting the x-label size
+            ax.set_yticklabels(y_values, size=axis_label_size) # Setting the y-label size
+            st.pyplot(fig) # Pasting the plot to streamlit
 
-            fig, ax = plt.subplots(figsize=(width, height))
-            ax.barh(y_values, x_values)
-            ax.set_title(f'{column}', size=title_font_size)
-            ax.set_xticks(range(max(x_values)+1), fontsize=axis_label_size)
-            ax.set_yticks(y_values, fontsize=axis_label_size)
-            print(axis_label_size)
-
-            st.pyplot(fig)
+            # Showing the actual data below
+            st.subheader('Actual Data')
+            st.dataframe(val_count_df.rename(columns={0:column})[::-1]) # Showing the dataframe
 
 
 
